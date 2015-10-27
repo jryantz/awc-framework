2b0407f8d543b359c229aa2f7b673f8c45b185b0
Jonathan Yantz <ijryantz@gmail.com>
2015-10-27 16:21:19 -0400

    added new user random password option
    added the option to be able to have a random password when creating a
    new user. this can only be used when creating a new user, as an admin.
    allows a more secure way to create large amounts of new users.

--- portal/user-new.php
+++ portal/user-new.php
@@ -38,7 +38,8 @@ $token = $Token->generate();
         <form action="" method="post">
             <input type="text" name="username" placeholder="Username">
             <input type="text" name="email" placeholder="Email">
-            <input type="password" name="password" id="pass" placeholder="Password">
+            <input type="password" name="password" id="password" placeholder="Password">
+            <input type="checkbox" name="random" id="random"><label for="random">Generate Random Password</label>
             <select name="rank">
                 <option value="" disabled selected>Select Rank</option>
                 <option value="0">Subscriber</option>
@@ -54,4 +55,43 @@ $token = $Token->generate();
     </body>
     
     <script src="js/passView.js"></script>
+    <script>
+        function passView() {
+    
+            if(document.getElementById('password').attributes['type'].value == 'password') {
+                document.getElementById('password').attributes['type'].value = 'text';
+            } else {
+
+                document.getElementById('password').attributes['type'].value = 'password';
+            }
+        }
+        
+        function randomPassword() {
+            
+            var result = '';
+            var length = 12;
+            var options = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            
+            for(var i = 0; i <= length; i++) {
+                result += options[Math.round(Math.random() * (options.length - 1))];
+            }
+            
+            return result;
+        }
+        
+        document.getElementById('random').addEventListener('click', function() {
+            
+            if(document.getElementById('random').checked) {
+                document.getElementById('password').attributes['type'].value = 'text';
+                document.getElementById('password').disabled = true;
+                document.getElementById('password').value = randomPassword();
+                
+            } else {
+                
+                document.getElementById('password').attributes['type'].value = 'password';
+                document.getElementById('password').disabled = false;
+                document.getElementById('password').value = '';
+            }
+        });
+    </script>
 </html>
