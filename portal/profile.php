7132b35ee46b15f1e952406ae0247bebea9f9112
Jonathan Yantz <ijryantz@gmail.com>
2017-03-14 11:07:05 -0400

    removed referenced to User_Name and made changes to clear out references to old code

--- portal/profile.php
+++ portal/profile.php
@@ -7,13 +7,8 @@ if(!isset($_SESSION['user'])) {
 }
 
 if(isset($_POST['updateName'])) {
-    $User = new User_Name;
+    $User = new User;
     $User->changeName();
-}
-
-if(isset($_POST['submitName'])) {
-    $User = new User_Name;
-    $User->createName();
 }
 
 $Token = new Token;
@@ -25,27 +20,26 @@ $token = $Token->generate();
 
 <html>
     <head>
-    
+
     </head>
-    
+
     <body>
         <?php
         if(isset($_SESSION['alert'])) {
             echo $_SESSION['alert'];
             unset($_SESSION['alert']);
         }
-        
+
         include_once('includes/menu.inc.php');
-        
+
         $User = new User;
         $data = $User->getData();
-        
+
         echo '<input type="text" name="username" value="' . $data[0]['username'] . '" disabled>Usernames cannot be changed.<br><br>';
-        
-        if(isset($data[1]['first']) && isset($data[1]['middle']) && isset($data[1]['last'])):
-        echo $data[1]['first'] . ' ' . $data[1]['middle'] . ' ' . $data[1]['last'];
+
+        echo $data[0]['first'] . ' ' . $data[0]['middle'] . ' ' . $data[0]['last'];
         ?>
-        
+
         <form action="" method="post">
             <input type="text" name="first" placeholder="First Name">
             <input type="text" name="middle" placeholder="Middle Name / MI">
@@ -53,17 +47,7 @@ $token = $Token->generate();
             <input type="hidden" name="token" value="<?php echo $token; ?>">
             <input type="submit" name="updateName" value="Update">
         </form>
-        <?php else: ?>
-        <form action="" method="post">
-            <input type="text" name="first" placeholder="First Name">
-            <input type="text" name="middle" placeholder="Middle Name / MI">
-            <input type="text" name="last" placeholder="Last Name">
-            <input type="hidden" name="token" value="<?php echo $token; ?>">
-            <input type="submit" name="submitName" value="Submit">
-        </form>
-        
-        <?php endif; ?>
-        
+
         <br>
         <a href="#">Change Password</a>
     </body>
