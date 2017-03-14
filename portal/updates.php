e70184485efb7ae13f551a3fe89067e572b54e6d
Jonathan Yantz <ijryantz@gmail.com>
2017-03-14 11:09:07 -0400

    added functionality to the updates page

--- portal/updates.php
+++ portal/updates.php
@@ -25,6 +25,27 @@ if(!isset($_SESSION['user'])) {
         include_once('includes/menu.inc.php');
         ?>
         
+        Updates
         
+        <br><br>Framework
+        
+        <?php
+        $data = file_get_contents('app/package.json');
+        $json = json_decode($data, true);
+        
+        $update = file_get_contents($json['update-link'] . '/update.json');
+        $updatetext = json_decode($update, true);
+        
+        echo '<br>Installed Version: ' . $json['version'];
+        
+        if(isset($updatetext['version'])) {
+            echo '<br>You can update to version ' . $updatetext['version'] . ' automatically by clicking the button below.';
+            echo '<br><a href="#">Update</a> &nbsp; <a href="#">Download ' . $updatetext['version'] . '</a>';
+        } else {
+            echo 'Error with update system.';
+        }
+        ?>
+        
+        <br><br>Packages
     </body>
 </html>
